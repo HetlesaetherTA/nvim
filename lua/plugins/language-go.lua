45dc918f10f15f805bf9da68c787529294928167
Thomas-Andre Hetlesaether <ta@hetlesaether.com>
2026-04-07 03:29:41 +0200

    add: dap-go (go debugger)

--- lua/plugins/language-go.lua
+++ lua/plugins/language-go.lua
@@ -16,6 +16,46 @@ return {
     end,
   },
 
+  -- Go debugger
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      "leoluz/nvim-dap-go",
+    },
+    config = function()
+      local dap = require("dap")
+      local ui = require("dapui")
+
+      require("dap-go").setup()
+      ui.setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+
+      vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+      vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue / Start" })
+      vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
+      vim.keymap.set("n", "<leader>dn", dap.step_over, { desc = "Step Over" })
+      vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "Step Out" })
+      vim.keymap.set("n", "<leader>dus", ui.toggle, { desc = "Toggle UI" })
+      vim.keymap.set("n", "<leader>dt", function()
+        require("dap-go").debug_test()
+      end, { desc = "Debug Test" })
+    end,
+  },
+
   -- install treesitter
   {
     "nvim-treesitter/nvim-treesitter",
